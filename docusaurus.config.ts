17394eea6d8d17946a7419135875ac99103e7136
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-04 01:21:29 +0900

    chore: 배포 설정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,13 @@ const config: Config = {
   url: 'https://gabrielyoon7.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/gabrielyoon7.dev/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'gabrielyoon7', // Usually your GitHub org/user name.
   projectName: 'gabrielyoon7.dev', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
